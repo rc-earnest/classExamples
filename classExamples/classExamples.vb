84d97c1a1b253bf74b5ec0e5f9396db3073a4748
rc-earnest <rc.earnest04@gmail.com>
2025-02-04 08:26:03 -0700

    made new sub and saw how to use variables in different subs

---
+++ classExamples/classExamples.vb
@@ -0,0 +1,24 @@
+﻿Option Explicit On
+Option Strict On
+Module classExamples
+
+    Sub Main()
+        Dim myLuckyNumber As Integer = 7
+        Console.WriteLine("Hello from Sub Main!")
+        Console.WriteLine($"{myLuckyNumber}")
+        MyOtherSub()
+        Sandwich(myLuckyNumber)
+    End Sub
+
+
+    Sub MyOtherSub()
+        Dim myLuckyNumber As Integer = 3
+        Console.WriteLine("Hello from MyOtherSub!")
+        Console.WriteLine($"{myLuckyNumber}")
+    End Sub
+
+    Sub Sandwich(ByVal myLuckyNumber As Integer)
+        Console.WriteLine("Hello from Subsandwich!")
+        Console.WriteLine($"{myLuckyNumber}")
+    End Sub
+End Module
