93b24a930f78d3e7dc90dfc023c514010ad3c854
rc-earnest <rc.earnest04@gmail.com>
2025-02-04 08:50:32 -0700

    new by ref sub and rng code

--- classExamples/classExamples.vb
+++ classExamples/classExamples.vb
@@ -4,21 +4,34 @@ Module classExamples
 
     Sub Main()
         Dim myLuckyNumber As Integer = 7
+        Dim random As Single
         Console.WriteLine("Hello from Sub Main!")
         Console.WriteLine($"{myLuckyNumber}")
-        MyOtherSub()
-        Sandwich(myLuckyNumber)
-    End Sub
-
-
-    Sub MyOtherSub()
-        Dim myLuckyNumber As Integer = 3
-        Console.WriteLine("Hello from MyOtherSub!")
+        'MyOtherSub()
+        'Sandwich(myLuckyNumber)
+        Nautica(myLuckyNumber)
+        Console.WriteLine("Hello from Sub Main!")
         Console.WriteLine($"{myLuckyNumber}")
+        random = Rnd()
+        Console.WriteLine($"{random * 5}")
     End Sub
 
-    Sub Sandwich(ByVal myLuckyNumber As Integer)
-        Console.WriteLine("Hello from Subsandwich!")
+
+    'Sub MyOtherSub()
+    '    Dim myLuckyNumber As Integer = 3
+    '    Console.WriteLine("Hello from MyOtherSub!")
+    '    Console.WriteLine($"{myLuckyNumber}")
+    'End Sub
+
+    'Sub Sandwich(ByVal myLuckyNumber As Integer)
+    '    myLuckyNumber += 5
+    '    Console.WriteLine("Hello from Subsandwich!")
+    '    Console.WriteLine($"{myLuckyNumber}")
+    'End Sub
+
+    Sub Nautica(ByRef myLuckyNumber As Integer)
+        'myLuckyNumber += 5
+        Console.WriteLine("Hello from Subnautica!")
         Console.WriteLine($"{myLuckyNumber}")
     End Sub
 End Module
