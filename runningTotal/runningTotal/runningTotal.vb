d8e42a0a5efe320bd48e77c3e3cb800f3396bd67
rc-earnest <rc.earnest04@gmail.com>
2025-02-19 08:38:46 -0700

    added currency format and can clear running total

--- runningTotal/runningTotal/runningTotal.vb
+++ runningTotal/runningTotal/runningTotal.vb
@@ -8,8 +8,8 @@ Option Explicit On
 'TODO
 '[x] keep track of transactions in a Function called RunningTotal()
 '[x] get the current total as needed
-'[] provide a way to clear/zero the total
-'[] display transactions and running total formatted as currency
+'[x] provide a way to clear/zero the total
+'[x] display transactions and running total formatted as currency
 '[] super bonus: create a method to include sales tax to the transaction amount
 Module runningTotal
     Dim myGlobalNumber As Decimal
@@ -18,34 +18,43 @@ Module runningTotal
         Dim quit As Boolean = False
         Dim transactionAmount As Decimal
         Do
-            Console.WriteLine("Enter a transaction amount or press q to quit")
+            Console.WriteLine("Enter a transaction amount.")
+            Console.WriteLine("Type q to quit.")
+            Console.WriteLine("Type t to check total.")
+            Console.WriteLine("Type c to clear.")
             userInput = Console.ReadLine()
+            Console.Clear()
             Try
                 transactionAmount = Decimal.Parse(userInput)
-                RunningTotal(transactionAmount)
+                RunningTotal(transactionAmount, False)
             Catch ex As Exception
+                Select Case userInput
+                    Case "q"
+                        quit = True
+                    Case "t"
+                        Console.Clear()
+                        Console.WriteLine($"The current total is {RunningTotal(0, False).ToString("C")}")
 
+                    Case "c"
+                        Console.Clear()
+                        RunningTotal(0, True)
+                End Select
 
-
-                If userInput = "q" Then
-                    quit = True
-                Else
-                    Console.WriteLine($"You entered: {userInput}")
-
-                End If
             End Try
         Loop Until quit
         Console.Clear()
-        Console.WriteLine($"The total is {RunningTotal(0)}")
+        Console.WriteLine($"The total is {RunningTotal(0, False).ToString("C")}")
         Console.WriteLine("Have a nice day!")
     End Sub
 
-    Function RunningTotal(currentNumber As Decimal) As Decimal
+    Function RunningTotal(currentNumber As Decimal, clear As Boolean) As Decimal
         Static _runningTotal As Decimal
-        _runningTotal += currentNumber
+        If clear Then
+            _runningTotal = 0
+        Else
+            _runningTotal += currentNumber
 
-
-
+        End If
 
         Return _runningTotal
     End Function
