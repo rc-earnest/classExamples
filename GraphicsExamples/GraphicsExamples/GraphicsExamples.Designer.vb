2c9ee501513fd2dbcea5d2bbca1da28bc290de08
rc-earnest <rc.earnest04@gmail.com>
2025-04-01 08:56:20 -0600

    Juice

---
+++ GraphicsExamples/GraphicsExamples/GraphicsExamples.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GraphicsExamples
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ForeGroundColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorDialog = New System.Windows.Forms.ColorDialog()
+        Me.MenuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip
+        '
+        Me.MenuStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip.Name = "MenuStrip"
+        Me.MenuStrip.Size = New System.Drawing.Size(800, 28)
+        Me.MenuStrip.TabIndex = 0
+        Me.MenuStrip.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ForeGroundColorToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(49, 24)
+        Me.EditToolStripMenuItem.Text = "Edit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(55, 24)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'ForeGroundColorToolStripMenuItem
+        '
+        Me.ForeGroundColorToolStripMenuItem.Name = "ForeGroundColorToolStripMenuItem"
+        Me.ForeGroundColorToolStripMenuItem.Size = New System.Drawing.Size(224, 26)
+        Me.ForeGroundColorToolStripMenuItem.Text = "Fore Ground Color"
+        '
+        'GraphicsExamples
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MenuStrip)
+        Me.MainMenuStrip = Me.MenuStrip
+        Me.Name = "GraphicsExamples"
+        Me.Text = "Form1"
+        Me.MenuStrip.ResumeLayout(False)
+        Me.MenuStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ForeGroundColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ColorDialog As ColorDialog
+End Class
---
+++ GraphicsExamples/GraphicsExamples/GraphicsExamples.vb
@@ -0,0 +1,82 @@
+﻿
+'Spring 2025
+Public Class GraphicsExamples
+    Function ForeGroundColor()
+
+    End Function
+
+    Sub DrawWithMouse(oldX As Integer, oldY As Integer, newX As Integer, newY As Integer)
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Black)
+
+
+        g.DrawLine(pen, oldX, oldY, newX, newY)
+
+        g.Dispose()
+    End Sub
+    Sub DrawLine()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Black)
+
+        'pen.Color = Color.Lime
+
+        g.DrawLine(pen, 50, 50, 100, 100)
+
+        g.Dispose()
+    End Sub
+
+    Sub DrawRectangle()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Red, 3)
+
+        g.DrawRectangle(pen, 150, 150, 300, 250)
+
+        g.Dispose()
+    End Sub
+
+    Sub DrawEllipse()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.RoyalBlue, 5)
+
+        g.DrawEllipse(pen, 160, 160, 280, 230)
+
+        g.Dispose()
+    End Sub
+
+    Sub DrawString()
+        Dim g As Graphics = Me.CreateGraphics
+        Dim pen As New Pen(Color.Red, 3)
+        ' Create font and brush.
+        Dim drawFont As New Font("Arial", 16)
+        Dim drawBrush As New SolidBrush(Color.Crimson)
+
+
+        g.DrawString("Graphics!", drawFont, drawBrush, 200, 200)
+
+        g.Dispose()
+    End Sub
+
+    ' Event Handlers ----------------------------------------------------------
+
+    Private Sub GraphicsExamples_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        Static oldX, oldY As Integer
+        Me.Text = $"({e.X},{e.Y})"
+        DrawWithMouse(oldX, oldY, e.X, e.Y)
+        oldX = e.X
+        oldY = e.Y
+    End Sub
+
+    Private Sub GraphicsExamples_Click(sender As Object, e As EventArgs) Handles Me.Click
+        DrawLine()
+        DrawRectangle()
+        DrawEllipse()
+        DrawString()
+    End Sub
+
+
+    'Private Sub ForeGroundColorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ForeGroundColorToolStripMenuItem.Click
+    '    Dim result As DialogResult = ColorDialog.ShowDialog()
+    '    MsgBox(result.ToString)
+    '    ForeGroundColor(ColorDialog.Color)
+    'End Sub
+End Class
